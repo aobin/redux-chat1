10d3e6db8f656b40614410cbfcbbfde8500c4776
aobin <ao.bin@hp.com>
2016-10-12 16:20:19 +0800

    Initial commit

---
+++ test/server/core_spec.js
@@ -0,0 +1,14 @@
+/**
+ * Created by aobin on 10/8/2016.
+ */
+import {expect} from "chai";
+import {v1} from "uuid";
+import {fromJS,Map,List} from "immutable";
+describe("first test", ()=>{
+    it("should work!",()=>{
+        console.log(v1()+1);
+    })
+
+})
+
+
---
+++ test/serverTestHelper.js
@@ -0,0 +1,11 @@
+/**
+ * Created by aobin on 10/8/2016.
+ */
+import chai from "chai";
+import chaiImmutable from "chai-immutable";
+
+chai.use(chaiImmutable);
+
+
+
+
